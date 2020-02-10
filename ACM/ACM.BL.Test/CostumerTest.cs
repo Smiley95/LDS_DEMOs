bea006639d22290014e96dd69d5796b21c6b0e87
Eya Taieb <ETaieb@se.linedata.com>
2020-02-10 15:20:26 +0100

    first commit

---
+++ ACM/ACM.BL.Test/CostumerTest.cs
@@ -0,0 +1,49 @@
+﻿//naming convention: CamelCase
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL.Test
+{
+    [TestClass]
+    public class CostumerTest
+    {
+        [TestMethod]
+        public void FullNameOutputTest()
+        {
+            //Arrange
+            Costumer costumer = new Costumer();
+            costumer.FirstName = "name";
+            costumer.LastName = "family name";
+            string expected = "name, family name";
+            //Act 
+            string actual = costumer._FullName;
+            //Assert 
+            Assert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void CountInstanceTest()
+        {
+            //Arrange
+            Costumer Frudo = new Costumer();
+            Frudo.countCustomers();
+            Costumer Lisa = new Costumer();
+            Frudo.countCustomers();
+            Costumer Jay = new Costumer();
+            Frudo.countCustomers();
+            //Assert
+            Assert.AreEqual(3, Costumer.CostumerCount);
+        }
+
+        [TestMethod]
+        public void ValidInputsTest()
+        {
+            //Arrange
+            Costumer costumerOne = new Costumer();
+            costumerOne.FirstName = "fname";
+            costumerOne.LastName = "lname";
+            costumerOne.EmailAddress = "lname@fname";
+            //Assert
+            Assert.IsTrue(costumerOne.Validate());
+        }
+    }
+}
---
+++ ACM/ACM.BL/Costumer.cs
@@ -0,0 +1,30 @@
+﻿//naming convention: CamelCase
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Costumer
+    {
+        public static int CostumerCount = 0;
+        public int CostumerID { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string _FullName
+        {
+            get
+            {
+                return FirstName + ", " + LastName;
+            }
+        }
+        public void countCustomers() { CostumerCount++; }
+        public bool Validate()
+        {
+            return (string.IsNullOrWhiteSpace(FirstName)|| string.IsNullOrWhiteSpace(LastName)|| string.IsNullOrWhiteSpace(EmailAddress))?  false: true;
+        }
+    }
+}
