2bce107cec3da8b7187c5bb555d56f7946d26b9e
Eya Taieb <ETaieb@se.linedata.com>
2020-02-20 15:18:52 +0100

    test for overdraft limit setter

--- Bank/AccountTest/AccountTest.cs
+++ Bank/AccountTest/AccountTest.cs
@@ -7,12 +7,18 @@ namespace AccountTest
 {
     public class AccountTest
     {
-
         [Fact]
         public void ShouldCreateAccount()
         {
             Account _account = new Account(4,"someone");
             Xunit.Assert.NotNull(_account);
+        }
+        [Fact]
+        public void ShouldSetOverdraftLimit()
+        {
+            Account _account = new Account(4, "someone");
+            _account.overdraftLimit = 10000;
+            Xunit.Assert.Equal(10000,_account.overdraftLimit);
         }
     }
 }
