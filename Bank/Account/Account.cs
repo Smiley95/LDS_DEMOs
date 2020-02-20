2b518f50335fa249d6160674c6a51487078d9f0e
Eya Taieb <ETaieb@se.linedata.com>
2020-02-20 16:54:39 +0100

    implement basic wire transfer method/ green Phase

--- Bank/Account/Account.cs
+++ Bank/Account/Account.cs
@@ -38,5 +38,9 @@ namespace BankAccount
             if(funds > 0 && funds<= balance) balance -= funds;
             else throw new Exception("Invalid fund input");
         }
+        public void WireTransfer(double funds)
+        {
+            balance -= funds;
+        }
     }   
 }
