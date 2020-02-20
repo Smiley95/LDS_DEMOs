66395f50d538a0725fde5afd724b7b8fc5564530
Eya Taieb <ETaieb@se.linedata.com>
2020-02-20 15:14:48 +0100

    implemeting the account constructor/ green phase

--- Bank/Account/Account.cs
+++ Bank/Account/Account.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Account
+namespace BankAccount
 {
     public class Account
     {
-
+        public int ID;
+        public string holder { get; set; }
+        public Account(int ID, string holder)
+        {
+            this.ID = ID;
+            this.holder = holder;
+        }
     }   
 
 }
--- Bank/AccountTest/AccountTest.cs
+++ Bank/AccountTest/AccountTest.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit;
+using BankAccount;
 
 namespace AccountTest
 {
     public class AccountTest
     {
+
         [Fact]
         public void ShouldCreateAccount()
         {
-            Account _account = new Account(4, "someone");
+            Account _account = new Account(4,"someone");
             Xunit.Assert.NotNull(_account);
         }
     }
