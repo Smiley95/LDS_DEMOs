09c9c46339fffffc75f3d47370f6314f5e317841
Eya Taieb <ETaieb@se.linedata.com>
2020-02-20 15:35:06 +0100

    implementing the deposit of cheque method/ green Phase

--- Bank/Account/Account.cs
+++ Bank/Account/Account.cs
@@ -7,6 +7,7 @@ namespace BankAccount
         public int ID;
         public string holder { get; set; }
         public double overdraftLimit { get; set; }
+        public double balance { get; set; }
         public  double dailyWireTransferLimit { get; set; }
         public Account(int ID, string holder)
         {
