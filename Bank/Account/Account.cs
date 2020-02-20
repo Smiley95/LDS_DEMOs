7712910a502c340189a2b1be92ea06ab972d5c21
Eya Taieb <ETaieb@se.linedata.com>
2020-02-20 15:21:05 +0100

    adding the overdraftLimit property

--- Bank/Account/Account.cs
+++ Bank/Account/Account.cs
@@ -6,11 +6,13 @@ namespace BankAccount
     {
         public int ID;
         public string holder { get; set; }
+        public double overdraftLimit { get; set; }
         public Account(int ID, string holder)
         {
             this.ID = ID;
             this.holder = holder;
         }
+
     }   
 
 }
